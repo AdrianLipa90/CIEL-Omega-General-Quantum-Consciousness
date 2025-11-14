a573a78f6decb4f06e05c784cae9564adba19da7
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 18:22:22 +0000

    Add copyright and license to forcing_field.py
    Added copyright and license information to the file.

--- bio/forcing_field.py
+++ bio/forcing_field.py
@@ -1,4 +1,10 @@
-"""Biological forcing field based on the intention vector."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Biological forcing field based on the intention vector.
+"""
 
 from __future__ import annotations
 
