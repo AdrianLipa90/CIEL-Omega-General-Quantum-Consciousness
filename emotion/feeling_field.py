1092430377320fa36597b0659eb8be156208823d
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 18:59:55 +0000

    Update feeling_field.py

--- emotion/feeling_field.py
+++ emotion/feeling_field.py
@@ -1,4 +1,10 @@
-"""Field level aggregation of emotional signals."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Field level aggregation of emotional signals.
+"""
 
 from __future__ import annotations
 
