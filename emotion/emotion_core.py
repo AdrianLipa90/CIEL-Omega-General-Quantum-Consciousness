e9d8d5359a671130448bb7d94cf588817cac6808
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 18:59:09 +0000

    Update emotion_core.py

--- emotion/emotion_core.py
+++ emotion/emotion_core.py
@@ -1,4 +1,10 @@
-"""Compact emotional core used by the high level tests."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Compact emotional core used by the high level tests.
+"""
 
 from __future__ import annotations
 
