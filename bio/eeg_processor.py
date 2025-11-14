e4cf3813b9e09dbe06e091ebb618963da4ee964a
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 18:21:41 +0000

    Add copyright and license to eeg_processor.py
    Added copyright information and license details to the EEG processor module.

--- bio/eeg_processor.py
+++ bio/eeg_processor.py
@@ -1,4 +1,10 @@
-"""Pre-process EEG traces for the rest of the pipeline."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Pre-process EEG traces for the rest of the pipeline.
+"""
 
 from __future__ import annotations
 
