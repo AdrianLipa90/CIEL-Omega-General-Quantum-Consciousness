d47d99612f76c7ae5bd059cb4ded05d2c487af1c
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 19:22:57 +0000

    Update lagrangian.py

--- evolution/lagrangian.py
+++ evolution/lagrangian.py
@@ -1,4 +1,10 @@
-"""Light-weight representation of the unified reality laws."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Light-weight representation of the unified reality laws.
+"""
 
 from __future__ import annotations
 
