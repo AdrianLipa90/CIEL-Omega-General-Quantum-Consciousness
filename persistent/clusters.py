d5e703dac58b6d54be1c5aeab15e6c8d8d5b58a6
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 21:36:13 +0000

    Update clusters.py

--- persistent/clusters.py
+++ persistent/clusters.py
@@ -1,4 +1,10 @@
-"""Simple in-memory representation of persistent clusters."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Simple in-memory representation of persistent clusters.
+"""
 from __future__ import annotations
 
 from dataclasses import dataclass, field
