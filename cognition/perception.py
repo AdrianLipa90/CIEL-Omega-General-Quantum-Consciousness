e19e1e1cb472306cd38639d43394dff6ca9d4447
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 18:36:13 +0000

    Add copyright and license details to perception.py
    Added copyright and licensing information to the file.

--- cognition/perception.py
+++ cognition/perception.py
@@ -1,4 +1,10 @@
-"""Light-weight perception layer using moving averages."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Light-weight perception layer using moving averages.
+"""
 
 from __future__ import annotations
 
