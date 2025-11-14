7ae7378146016fcb1ba778922cab59c2831996e1
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 18:25:11 +0000

    Update copyright and license information in __init__.py

--- ciel_memory/__init__.py
+++ ciel_memory/__init__.py
@@ -1,4 +1,10 @@
-"""Public surface of the lightweight CIEL memory utilities."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Public surface of the lightweight CIEL memory utilities.
+"""
 
 from .orchestrator import UnifiedMemoryOrchestrator
 
