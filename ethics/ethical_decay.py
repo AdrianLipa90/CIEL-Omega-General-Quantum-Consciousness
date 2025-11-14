79af7ba52b817f3683a0d00136fca4e6b30443bb
AdrianLipa90 <adrianlipa90@gmail.com>
2025-11-14 19:02:30 +0000

    Update ethical_decay.py

--- ethics/ethical_decay.py
+++ ethics/ethical_decay.py
@@ -1,4 +1,10 @@
-"""Utility describing an exponential decay of ethical certainty."""
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+
+Utility describing an exponential decay of ethical certainty.
+"""
 
 from __future__ import annotations
 
