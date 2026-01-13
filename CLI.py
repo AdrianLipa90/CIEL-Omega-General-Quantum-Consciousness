6ec1dc2b51457015caa44eca0e489d2dd2cbe13b
Adrian Lipa <adrianlipa90@gmail.com>
2026-01-13 14:28:16 +0000

    Add header comment and copyright information

--- CLI.py
+++ CLI.py
@@ -1,3 +1,9 @@
+"""CIEL/Ω Quantum Consciousness Suite
+
+Copyright (c) 2025 Adrian Lipa / Intention Lab
+Licensed under the CIEL Research Non-Commercial License v1.1.
+Created for Intention Lab by Mikołaj Boruc"""
+
 import sys
 
 if __name__ == "__main__":
